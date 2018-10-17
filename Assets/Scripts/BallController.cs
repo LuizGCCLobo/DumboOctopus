64e5913c6293241254b9ea3d369f424935a6274c
Luiz Lobo <luizguilhermecclobo@gmail.com>
2018-10-17 10:16:50 -0300

    Adding ball project
    Sample scene + main controls scripts
    
    Next up is making the ball turn everyone...
    
    Not sure if will keep the jump

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    [SerializeField] bool isFlat = true;
+    private Rigidbody rigid;
+
+    [SerializeField] float speed;
+    private float velocity;
+    [SerializeField] bool grounded = false;
+    
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        MovingBall();
+    }
+
+    void MovingBall()
+    {
+        Vector3 tilt = Input.acceleration;
+    
+        if (isFlat)
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+
+        rigid.AddForce(tilt);
+
+        if (Input.GetMouseButtonDown(0) && grounded)
+        {
+            rigid.AddForce(Vector3.up * speed);
+            grounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag.Equals("Ground"))
+            grounded = true;
+    }
+}
---
+++ Assets/Scripts/BallParentController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallParentController : MonoBehaviour {
+
+    [SerializeField] GameObject ballChild;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = ballChild.transform.position;
+	}
+}
